70aee49f185215d7dca108543b8fad9bf9be0b45
Brian Dominguez Vazquez <brian@saascreations.mx>
2025-01-16 11:45:39 -0600

    layout base :smile:

--- src/config/navigationBar.ts
+++ src/config/navigationBar.ts
@@ -34,24 +34,24 @@ export interface NavData {
 export const navigationBarData: NavData = {
 	logo: {
 		src: '/logo.svg',
-		alt: 'The tailwind astro theme',
-		text: 'Foxi.'
+		alt: 'Mamás Autónomas',
+		text: 'Mamás Autónomas.'
 	},
 	navItems: [
-		{ name: 'Home', link: '/' },
-		{ name: 'Pricing', link: '/pricing' },
-		{ name: 'Features', link: '/features' },
-		{
-			name: 'Resources',
-			link: '#',
-			submenu: [
-				{ name: 'Blog', link: '/blog' },
-				{ name: 'Changelog', link: '/changelog' },
-				{ name: 'FAQ', link: '/faq' },
-				{ name: 'Terms', link: '/terms' }
-			]
-		},
-		{ name: 'Contact', link: '/contact' }
+		{ name: 'Inicio', link: '/' },
+		// { name: 'Pricing', link: '/pricing' },
+		{ name: 'Nuestros beneficios', link: '/features' },
+		// {
+		// 	name: 'Resources',
+		// 	link: '#',
+		// 	submenu: [
+		// 		{ name: 'Blog', link: '/blog' },
+		// 		{ name: 'Changelog', link: '/changelog' },
+		// 		{ name: 'FAQ', link: '/faq' },
+		// 		{ name: 'Terms', link: '/terms' }
+		// 	]
+		// },
+		{ name: 'Contacto', link: '/contact' }
 	],
-	navActions: [{ name: 'Try it now', link: '/', style: 'primary', size: 'lg' }]
+	navActions: [{ name: '¡Únete a nosotras!', link: '/', style: 'primary', size: 'lg' }]
 }
