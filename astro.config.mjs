dce79d98a5d3d27a8ece77248b76eb1280e36e4b
Brian Dominguez Vazquez <brian@saascreations.mx>
2025-01-16 15:48:13 -0600

    link

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://mamas-autonomas.netlify.app/",
   integrations: [
     tailwind(),
     icon(),
@@ -19,3 +19,4 @@ export default defineConfig({
     }),
   ],
 });
+
